6957605d458643796ccc6747ca12f682832f18bd
Vaibhav Arora <varora@nd.edu>
2021-04-19 17:21:31 -0400

    Made json import from file

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ import json
 from pprint import pprint
 from quick_sort import quick_sort_func #allows us to call the quick_sort python code
 
-
-URL = "https://raw.githubusercontent.com/varora24/Find-My-Game/data/games.json"
+file_name = "games.json"
+#URL = "https://raw.githubusercontent.com/varora24/Find-My-Game/data/games.json"
 
 def main():
 
-    response = requests.get(URL).json() # accepts response from json file on github which contains information about all games
+    response = open(file_name,) #requests.get(URL).json() # accepts response from json file on github which contains information about all games
+    data = json.load(response);
+    pprint(data)
     
-    pprint(response)
-    
-    quick_sort_func(response, 0, len(response) - 1, 'Maxplayers',False)
+    quick_sort_func(data, 0, len(data) - 1, 'Minplayers')
 
     pprint(response)
 
