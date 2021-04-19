cbd9dbd9e542012ec87e0070203579337b293bf3
Vaibhav Arora <varora@nd.edu>
2021-04-19 18:55:14 -0400

    count_sort

--- count_sort.py
+++ count_sort.py
@@ -1,33 +1,37 @@
 import math
 import os
 import sys
+import pprint
 
 def count_sort_func(data,maxdata,index):
    maxdata +=1
    count_list = [0]*(maxdata)
-   count_dict = [0]*len(data)
-   print(count_dict)
+   count_dict = data
+   #print(count_dict)
    #print(count_list)
    for n in data:
-      #print(n[index])
-      #exit()
+      
       count_list[n[index]] +=1 # count_list[n[index]] + 1
 
    i = 0
+   count = 0
    for n in range(len(count_list)):
       #print(n)
       #exit()
       while(count_list[n]>0):
-         data[i] = n
+         for vals in count_dict:
+            j = 0
+            if vals[index]==n:
+               pprint.pprint(vals)
+               count+=1
+               print(count)
+               data[i] = vals
+               count_dict.pop(j)
+            j+=1
+         #data[i] = n
          i+=1
          count_list[n] -= 1
-   print("Hi")
-   print(list(data))
+   #pprint(list(data))
 
    return data
-
-
-if __name__ == "__main__":
-   main()
-
 
