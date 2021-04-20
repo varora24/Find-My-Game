7515c6c9c529bca5f6216be9aaade187b4eb4f3e
Vaibhav Arora <varora@nd.edu>
2021-04-20 10:34:20 -0400

    Added default values if values missing in data

--- main.py
+++ main.py
@@ -5,28 +5,22 @@ import sys
 import requests
 import json
 from pprint import pprint
-from quick_sort import quick_sort_func #allows us to call the quick_sort python code
 from count_sort import count_sort_func
 from bubble_sort import bubble_sort_func
 
 file_name = "games.json"
-#URL = "https://raw.githubusercontent.com/varora24/Find-My-Game/data/games.json"
-
+index = 'Minplayers'
 def main():
-
-    response = open(file_name,) #requests.get(URL).json() # accepts response from json file on github which contains information about all games
+    response = open(file_name,)# accepts response from json file on github which contains information about all games
     data = json.load(response);
     maxval = 1
-    index = 'Minplayers'
     for i in data:
        if i[index] > maxval:
           maxval = i[index]
     data = bubble_sort_func(data,index)
     #data = count_sort_func(data,maxval,index)
     #pprint (sorted(data, key = lambda i: i['Minplayers']))
-    #data = quick_sort_func(data, 0, len(data) - 1, 'Minplayers')
-
-    pprint(data)
+    #pprint(data)
 
 if __name__ == '__main__':
 	main()
