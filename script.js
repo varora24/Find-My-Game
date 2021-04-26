703adf0af5e6191505841108e06a0d1f633fa42d
Vaibhav Arora <72528539+varora24@users.noreply.github.com>
2021-04-26 09:58:16 -0400

    Update script.js
    User form input from html stored as variables

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 var a;
 function showbox(){
-	if(a==0){
+	//if(a==0){
 		document.getElementById("backbox").style.display="inline";
-		return a=1;
-	}
-	else{
-		document.getElementById("backbox").style.display="none";
-		return a=0;
-	}
+		console.log(numplayers);
+	//	return a=1;
+	//}
+	//else{
+		//document.getElementById("backbox").style.display="none";
+	//	return a=0;
+	//}
+}
+
+function submitEvent(){
+	var numplayers = document.getElementById("numplayers").value;
+	var genre = document.getElementById("genre").value;
+	var mode = document.getElementById("mode").value;
+
+	showbox();
 }
 
