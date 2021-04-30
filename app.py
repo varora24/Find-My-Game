cbce88717219809a3b1a2891d103c46c300fcc01
Gracie Molnar <gmolnar1@nd.edu>
2021-04-30 16:13:44 -0400

    app 2

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 import requests
 import json 
+import os
+import sys
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -16,6 +18,8 @@ dataout = []
 @app.route('/', methods=['POST','GET'])
 def index():
 
+    data = bubble_sort_func(data, 'Maxplayers')
+
     if request.method == 'GET': # GET request is sent when html wants some information from the python script
         return render_template('index.html')
     
@@ -24,13 +28,14 @@ def index():
         genre = request.form.get('genre')
         mode = request.form.get('mode')
 
-        findmatches(numplayers,genre,mode)
+        findmatches(numplayers, genre, mode)
         
         print(dataout)
 
     return render_template("index.html")
 
-def findmatches(numplayers,genre,mode):
+
+def findmatches(numplayers, genre, mode):
 
     if not numplayers:
         numplayers = 1
@@ -60,3 +65,41 @@ def findmatches(numplayers,genre,mode):
             if modematch == True and genrematch == True:
                 dataout.append(games)
 
+
+def check_valid(value):
+
+    if not value['Title']:
+        value['Title'] = 'No name'
+    if not value['Minplayers']:
+        value['Minplayers'] = 1
+    if not value['Maxplayers']:
+        value['Maxplayers'] = 2
+    if not value['Category']:
+        value['Category'] = 'Fun'
+    if not value['Description']:
+        value['Description'] = 'No description'
+    if not value['Time']
+        value['Time'] = '30 min'
+    if not value['Price']
+        value['Price'] = '0'
+    if not value['Format']:
+        value['Format'] = 'Website'
+    if not value['URL']:
+        value['URL'] = 'No URL'
+    if not value['Picture']:
+        value['Picture'] = 'https://i.ibb.co/qxjcPtZ/Untitled-design.png'
+
+    return value
+
+
+def bubble_sort_func(data, index):
+    
+    length = len(data)
+
+    for i in range(length-1):
+        data[i] = check_valid(data[i])
+        for j in range(0, length - i - 1):
+            if data[j][index] > data[j + 1][index]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+
+    return data
