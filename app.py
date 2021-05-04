c57162a1556a45f7921526366558c9a2c13730d5
Sean Beckerle <sbeckerl@nd.edu>
2021-05-04 14:56:29 -0400

    bubblesort update

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def bubble_sort_func(data, index):
     length = len(data)
 
     for i in range(length-1):
-        data[i] = check_valid(data[i])
+        check_valid(data[i])
         for j in range(0, length - i - 1):
             if data[j][index] > data[j + 1][index]:
                 data[j], data[j + 1] = data[j + 1], data[j]
